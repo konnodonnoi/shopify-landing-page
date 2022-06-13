035a336294d5ca94b529d5a66c445e8cbcf90c5a
konnodonnoi <saboanamero8521@gmail.com>
2022-06-12 20:14:20 -1200

    first

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+import './App.css';
+
+const App = () => 
+   
+    <div className="container mt-4">
+      <h3 className='display-4 text-center mb-4'>
+      <i className="fa-brands fa-accusoft"></i>Shopify Landing Page
+      </h3>
+<FileUpload />
+
+
+    </div>;
+
+
+export default App;
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fileUpload = require('express-fileUpload');
+
+const app = express ();
+
+app.use(fileUpload());
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://localhost:3000');
+    next();
+};
+
+//Uploading Endpoint
+app.post('/upload', allowCrossDomain, (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({msg: 'No file uploaded'});
+
+    };
+
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/upload/${file.name}`, err => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ fileName: file.name, filePath: `/upload/${file.name}`});
+
+        
+    });
+})
+
+
+app.listen(5000, () => console.log('🚀 server up and ready!')); 
